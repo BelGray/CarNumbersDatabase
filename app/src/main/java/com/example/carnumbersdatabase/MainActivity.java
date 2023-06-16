b2884345a9bae7a2323497fc9449c39868a85ff1
BelGray <goldendaniel186@gmail.com>
2023-06-16 08:25:13 +0300

    add test listener

--- app/src/main/java/com/example/carnumbersdatabase/MainActivity.java
+++ app/src/main/java/com/example/carnumbersdatabase/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.carnumbersdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button searchButton;
+    private Button addNumbersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(this);
+
     }
+
+    public void onClick(View v){
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_show_numbers_info);
+            }
+        });
+    }
+
 }
