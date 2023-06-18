da2896eabeefd31fc12c4f934961e38805e6c191
BelGray <goldendaniel186@gmail.com>
2023-06-18 18:09:50 +0300

    add field errors

--- app/src/main/java/com/example/carnumbersdatabase/activities/MainActivity.java
+++ app/src/main/java/com/example/carnumbersdatabase/activities/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.carnumbersdatabase.activities;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
 import com.example.carnumbersdatabase.R;
+import com.example.carnumbersdatabase.database.Numbers;
+import com.example.carnumbersdatabase.database.NumbersDatabase;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,38 +30,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        NumbersDatabase numbersDatabase = NumbersDatabase.getInstance(this);
+        Pattern cyrillic = Pattern.compile("^[а-яА-Я]+$");
+        Pattern latin = Pattern.compile("^[a-zA-Z]+$");
+        String rangeRegex = "[0-9]+";
         Button searchButton = findViewById(R.id.searchButton);
         Button addNumbersButton = findViewById(R.id.addNumbersButton);
+        TextInputLayout searchCarNumbersEditText = findViewById(R.id.searchCarNumbersEditText);
+        TextInputLayout searchRegionCodeEditText = findViewById(R.id.searchRegionCodeEditText);
+        TextView searchNumbersTextView = findViewById(R.id.searchNumbersTextView);
 
-        // Обработка нажатий на любой UI происходит в onCreate.
-        // Каждой кнопке свой отдельный интерфейс View.OnClickListener
-        searchButton.setOnClickListener(new View.OnClickListener() { // Объекту searchButton мы вызываем метод setOnClickListener, в который мы передаем реализацию интерфейса. Это называет механизм обратного вызова
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { // v - параметр! (2)
-                // Вот здесь происходит действие при нажатии на кнопку
-                // Каждой отдельной кнопке совоя собственная обработка нажатия. У
-                // тебя в прграмме не будет же милиоон кнопок на которые наживыешь и происходит
-                // одно и то же действие
+            public void onClick(View v) {
+                String regionCode = searchRegionCodeEditText.getEditText().getText().toString().trim();
+                String carNumbers = searchCarNumbersEditText.getEditText().getText().toString().trim();
+                Matcher carNumbersCyrillic = cyrillic.matcher(carNumbers);
+                Matcher carNumbersLatin = latin.matcher(carNumbers);
+                boolean searchCarNumbersFieldStatusOk;
+                boolean searchRegionCodeFieldStatusOk;
+
+                if (regionCode.isEmpty() || regionCode.length() > 3){
+                    searchRegionCodeFieldStatusOk = false;
+                    searchRegionCodeEditText.setError("Некорректный код региона!");
+                } else {
+                    searchRegionCodeFieldStatusOk = true;
+                    searchRegionCodeEditText.setError(null);
+                }
 
 
-                // У тебя тут будет переход на другую активити ShowNumbersInfoActivity
-                // вот так происходит нормальный переход на другую активити.
-                Intent goNumbersInfo = new Intent(MainActivity.this, ShowNumbersInfoActivity.class);
-                startActivity(goNumbersInfo);
+                if (carNumbers.length() != 6 || !carNumbersCyrillic.find() && carNumbersLatin.find() || carNumbersLatin.find() || !(carNumbers.substring(1, 4).matches(rangeRegex)) || carNumbers.matches(rangeRegex)){
+                    searchCarNumbersFieldStatusOk = false;
+                    searchCarNumbersEditText.setError("Некорректные номера автомобиля. Пример: C777PУ");
+                }
+                else {
+                    searchCarNumbersFieldStatusOk = true;
+                    searchCarNumbersEditText.setError(null);
+                }
+
+                if (searchCarNumbersFieldStatusOk && searchRegionCodeFieldStatusOk) {
+                    searchNumbersTextView.setText("Поиск по базе");
+                    searchNumbersTextView.setTextColor(Color.parseColor("#142899"));
+                    try {
+                        LiveData<List<Numbers>> numbersList = numbersDatabase.numbersDao().getPersonByCarNumbers(carNumbers, Integer.parseInt(regionCode));
+                    } catch (Exception exception){
+                        searchNumbersTextView.setText("Не найдено");
+                        searchNumbersTextView.setTextColor(Color.parseColor("#AE1D1D"));
+                    }
+                }
+
+
+                //Intent goNumbersInfo = new Intent(MainActivity.this, ShowNumbersInfoActivity.class);
+                //startActivity(goNumbersInfo);
             }
         });
 
-        // внутри каждой активити свой собственный набор UI элементов, значит внутри каждой активити нужно реализовывать нажатие на кнопку.
-        // Обработаем нажатие на другую кнопку addNumbersButton.
-
-        // (2)
-        // Точно так же - вызываем метод addNumbersButton, только в параметр не будем пихать интрейфейс - это тоже
-        // уже устаревший метод. В параметр мы передаем лямбда функцию. В методе onClick(View v) один параметр - v
-        // занчит просто пишем (v ->) то что будет после стрелочки - реализация метода onClick.
         addNumbersButton.setOnClickListener(v -> {
-            // Все работает абсолютно идентично searchButton. Мы делаем реализацию того же метода,
-            // только на современный лад.
 
             Toast.makeText(MainActivity.this, "ПРОИЗОШЛО НАЖАТИЕ НА КНОПКУ!", Toast.LENGTH_SHORT).show();
 
